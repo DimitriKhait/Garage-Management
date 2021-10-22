4412541214e91c713e185797c78331fedb55c9f7
Dimitri Khait <80476663+DimaKhait@users.noreply.github.com>
2021-10-22 18:05:43 +0300

    Add files via upload

---
+++ C21 Ex03 OrpazJoseph 205702350 DimitriKhait 318050945/C21 Ex03 OrpazJoseph 205702350 DimitriKhait 318050945/Ex03.GarageLogic/AllVehicleInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class AllVehicleInfo
+    {
+        public string m_ModelName;
+        public string m_LicenseNumber;
+        public EnterVehicle.eVehicleType m_VehicleType;
+        public Car.eColor m_Color;
+        public Car.eNumOfDoors m_Doors;
+        public Motorcycle.eLicenseType m_LicenseType;
+        public int m_EngineCapacity;
+        public bool m_DangerousGoods;
+        public float m_CargoCapacity;
+        public float m_CurrentLitersOfGas;
+        public float m_RemainingHoursOfBattery;
+        public string m_WheelsManufacturerName;
+        public float m_CurrentAirPressure;
+
+        // $G$ NTT-005 (-10) Should have used getters and setters, instead you're applying data in the UI which doesn't seperate UI from Logics at all
+    }
+}
